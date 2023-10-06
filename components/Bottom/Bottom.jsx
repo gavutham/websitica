c1fa8df5dba61714886027cb6a0b9ce8b692d5b0
Skanta <skantasamvartan825@gmail.com>
2023-10-06 15:11:10 +0530

    bottom first commmit

--- components/Bottom/Bottom.jsx
+++ components/Bottom/Bottom.jsx
@@ -1,7 +1,72 @@
 import "./Bottom.scss";
 
 const Bottom = () => {
-  return <div>Bottom</div>;
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+  };
+
+  const flexItemStyle1 = {
+    flex: 2, 
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: 'rgba(240, 240, 240, 0.5)',
+    padding: '20px',
+    border: '1px solid #ccc',
+  };
+
+  const imageStyle = {
+    maxWidth: '100%',
+    marginRight: '20px',
+  };
+
+  const paragraphStyle = {
+    flex: 1, 
+  };
+
+  const firstParagraphStyle = {
+    fontSize: '80%',
+  };
+
+  const secondParagraphStyle = {
+    fontSize: '60%', 
+  };
+
+  const flexItemStyle2 = {
+    flex: 7,
+    backgroundColor: 'rgba(240, 240, 240, 0.5)', 
+    padding: '20px',
+    border: '1px solid #ccc',
+  };
+
+  const flexItemStyle3 = {
+    flex: 3, 
+    backgroundColor: 'rgba(240, 240, 240, 0.5)',
+    padding: '20px',
+    border: '1px solid #ccc',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={flexItemStyle1}>
+        <img src="/Rectangle 15.png" alt="face icon" style={imageStyle} />
+        <div style={paragraphStyle}>
+          <p style={firstParagraphStyle}>Seasons in</p>
+          <p style={secondParagraphStyle}>James</p>
+        </div>
+      </div>
+      <div style={flexItemStyle2}>
+        <hr />
+        7/12
+      </div>
+      <div style={flexItemStyle3}>
+        3/12
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default Bottom;
+
+

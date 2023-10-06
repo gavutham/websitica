cbe975d69d12190cd7f45de7f15fac348d8a64f4
Gavutham <kgavutham2005@gmail.com>
2023-10-06 13:43:26 +0530

    basic mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <h1>Ohh oh! Page not Found!</h1>,
+  },
+]);
+
 function App() {
-  return <>Hello Mf</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

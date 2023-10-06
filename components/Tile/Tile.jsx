28bb1f192a7e29f8838da7bf39a741689d350c7a
Gavutham <kgavutham2005@gmail.com>
2023-10-06 15:23:57 +0530

    top section

---
+++ components/Tile/Tile.jsx
@@ -0,0 +1,7 @@
+import "./Tile.scss";
+
+const Tile = () => {
+  return <div>Tile</div>;
+};
+
+export default Tile;
--- components/Top/Top.jsx
+++ components/Top/Top.jsx
@@ -1,7 +1,48 @@
 import "./Top.scss";
+import { AiOutlineSearch, AiFillPlayCircle, AiFillHeart } from "react-icons/ai";
+import { TbMusicPlus } from "react-icons/tb";
 
 const Top = () => {
-  return <div>Top</div>;
+  return (
+    <div className="top">
+      <div className="top">
+        <div className="icon">
+          <AiOutlineSearch size={20} color="grey" />
+        </div>
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <img src="/Lead-image.png" alt="" />
+        </div>
+        <div className="right">
+          <div className="title">{"Tommorrow's Tune"}</div>
+          <div className="desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </div>
+          <div className="info">64 songs - 16 hrs+</div>
+          <div className="buttons">
+            <div className="">
+              <div className="icon">
+                <AiFillPlayCircle />
+              </div>
+              Play all
+            </div>
+            <div className="">
+              <div className="icon">
+                <TbMusicPlus />
+              </div>
+              Add to Collection
+            </div>
+            <div className="">
+              <AiFillHeart color="red" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Top;

c214664680f8cc8d38eed7f8765cdefadcffd2b8
Gavutham <kgavutham2005@gmail.com>
2023-10-06 15:41:14 +0530

    center

--- components/Center/Center.jsx
+++ components/Center/Center.jsx
@@ -1,7 +1,53 @@
+import Tile from "../Tile/Tile";
 import "./Center.scss";
 
+const data = [
+  {
+    title: "Let Me Love You - Krix",
+    artist: "Single",
+    time: "4:17",
+    img: "/Rectangle 25.png",
+  },
+  {
+    title: "Wantin Man go d0 - Burna",
+    artist: "African Giant",
+    time: "2:30",
+    img: "/Rectangle 24.png",
+  },
+  {
+    title: "Stand Strong",
+    artist: "Single",
+    time: "2:02",
+    img: "/Rectangle 23.png",
+  },
+  {
+    title: "Closa-Ybee",
+    artist: "Obi datti",
+    time: "3:23",
+    img: "/Rectangle 22.png",
+  },
+  {
+    title: "Let Me Love You - Krix",
+    artist: "Single",
+    time: "4:17",
+    img: "/Rectangle 21.png",
+  },
+];
+
 const Center = () => {
-  return <div>Center</div>;
+  return (
+    <div className="center">
+      {data.map((d) => (
+        <Tile
+          key={d.title}
+          title={d.title}
+          artist={d.artist}
+          path={d.img}
+          time={d.time}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Center;
--- components/Tile/Tile.jsx
+++ components/Tile/Tile.jsx
@@ -1,7 +1,26 @@
 import "./Tile.scss";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
-const Tile = () => {
-  return <div>Tile</div>;
+const Tile = ({ path, title, artist, time }) => {
+  return (
+    <div className="tile">
+      <div className="first">
+        <img src={path} alt="" />
+      </div>
+      <div className="second">
+        <div className="">{title}</div>
+      </div>
+      <div className="third">
+        <div className="">{artist}</div>
+      </div>
+      <div className="fourth">
+        <div className="">{time}</div>
+      </div>
+      <div className="fifth">
+        <BiDotsVerticalRounded />
+      </div>
+    </div>
+  );
 };
 
 export default Tile;
